6474881f990b5490cc873f801ffbd5f2bebb9714
SenanS <30498489+SenanS@users.noreply.github.com>
2021-03-28 23:15:43 +0100

    Refactored variables for understandability

--- Lab4.runs/impl_1/rundef.js
+++ Lab4.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "write_bitstream", "begin" );
+ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
          "-log calculator_top_module.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source calculator_top_module.tcl -notrace" );
 
